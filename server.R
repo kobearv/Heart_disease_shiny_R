c56420c788493d87b93e80010533c6df90003c30
sharkla <sharklalw@gmail.com>
2016-05-22 11:03:06 +0200

    Update server.R

--- server.R
+++ server.R
@@ -16,10 +16,20 @@ result<-eventReactive(input$Submit,
 pred_system<-predict(model_rf,k,method="class",type="prob")
 pred_result<-as.data.frame(pred_system)
 
+#output$plot<-hist(pred_result)
+#output$input_result<-renderTable({paste(pred_result)})
+pred_result$positive
+})
+output$text<-renderPrint({paste("The probrability of heart disease :",result())})
+#output$input_result<- renderDataTable({
+  #col.names <- c("negative", "positive")
+  #cbind(Outcome = result, as.data.frame(, ncol = 2))})
+
 
 })
     
-output$text<-renderPrint({paste("The results of this patients: ",result(),"negative/positive probability")})
+
+   
 
 
 
